ecccacc9e5dae37cce9f2ac211848dca9439c710
maxkvant <max.kvant@gmail.com>
2020-04-01 19:29:04 +0300

    fix __init__.py

--- src/pytorch_impl/estimators/__init__.py
+++ src/pytorch_impl/estimators/__init__.py
@@ -1,3 +1,4 @@
 from pytorch_impl.estimators.sgd_estimator import SgdEstimator
 from pytorch_impl.estimators.linearized_sgd_estimator import LinearizedSgdEstimator
 from pytorch_impl.estimators.matrix_exp_estimator_ import MatrixExpEstimator
+from pytorch_impl.estimators.bagging_estimator import BaggingEstimator
