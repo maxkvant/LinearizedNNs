fix gradient boosting estimator

src/pytorch_impl/estimators/gradient_boosting_estimator.py
@@ -16,7 +16,7 @@ class GradientBoostingEstimator(Estimator):
 
         self.ws_change_sum = None
         self.ys = []
-        self.y_prediction_changes = []
+        self.Xs = []
         self.y_predictions_before = []
 
         self.learning_rate = self.base_estimator.get_learning_rate()
@@ -24,15 +24,15 @@ class GradientBoostingEstimator(Estimator):
     def fit_batch(self, X, y):
         cur_estimator = self.new_partial_estimator()
 
-        y_pred = self.predict(X).detach()
+        y_pred = cur_estimator.predict(X).detach()
         y_residual = self.find_y_residual(y_pred, y)
 
-        pred_change = cur_estimator._fit_batch(X, y_residual)
+        cur_estimator.fit_residuals(X, y_residual)
         cur_ws_change = (cur_estimator.ws - self.base_estimator.ws).detach()
 
         self.ys.append(y)
-        self.y_predictions_before.append(y_pred)
-        self.y_prediction_changes.append(pred_change)
+        self.Xs.append(X)
+        self.y_predictions_before.append(y_pred.detach())
         self.ws_change_sum = cur_ws_change if (self.ws_change_sum is None) else (self.ws_change_sum + cur_ws_change)
 
     def set_learning_rate(self, learning_rate):
@@ -45,18 +45,19 @@ class GradientBoostingEstimator(Estimator):
         estimator = self.get_current_estimator()
         return estimator.predict(X)
 
-    def epoch_callback(self):
+    def step(self):
         self.base_estimator = self.get_current_estimator()
 
         self.ws_change_sum = None
         self.ys = []
-        self.y_prediction_changes = []
+        self.Xs = []
         self.y_predictions_before = []
 
     def find_y_residual(self, y_pred, y):
         y_pred.requires_grad = True
         loss = self.criterion(y_pred, y)
         loss.backward()
+        print(f"accuracy before fit {(y_pred.argmax(dim=1) == y).float().mean().item():.5f}, loss {loss.item():.5f}")
 
         y_residual = y_pred.grad
         y_diff = y_pred - to_one_hot(y, self.num_classes).to(self.device)
@@ -67,13 +68,18 @@ class GradientBoostingEstimator(Estimator):
         if self.ws_change_sum is None:
             return self.base_estimator
 
+        l = len(self.ys)
         estimator: MatrixExpEstimator = self.estimator_constructor()
         estimator.set_learning_rate(self.learning_rate)
+        estimator.set_ws(self.base_estimator.ws + self.ws_change_sum / l)
 
-        l = len(self.ys)
+        y_prediction_changes = [(estimator.predict(X) - pred_before).detach()
+                                for X, pred_before in zip(self.Xs, self.y_predictions_before)]
+
         beta = self.find_beta(torch.cat(self.y_predictions_before),
-                              torch.cat(self.y_prediction_changes),
+                              torch.cat(y_prediction_changes),
                               torch.cat(self.ys))
+
         print(f"beta {beta}")
 
         new_ws = self.base_estimator.ws + beta * self.ws_change_sum / l
@@ -86,7 +92,7 @@ class GradientBoostingEstimator(Estimator):
         estimator.set_learning_rate(self.learning_rate)
         return estimator
 
-    def find_beta(self, y_pred, pred_change, y, n_iter=500):
+    def find_beta(self, y_pred, pred_change, y, n_iter=1000):
         beta = torch.tensor(1.).to(self.device)
         beta.requires_grad = True
         learning_rate = 0.01

src/pytorch_impl/estimators/matrix_exp_estimator_.py
@@ -53,15 +53,17 @@ class MatrixExpEstimator(Estimator):
         loss.backward()
         y_residual = y_pred.grad
 
+        print(f"accuracy before fit {(y_pred.argmax(dim=1) == y).float().mean().item():.5f}, loss {loss.item():.5f}")
+
         y_diff = y_pred - to_one_hot(y, self.num_classes).to(self.device)
         scale = (y_diff * y_residual).sum() / (y_residual ** 2).sum()
 
-        self._fit_batch(X, y_residual * scale)
+        self.fit_residuals(X, y_residual * scale)
 
         print(f"fitting done. took {time.time() - start_time:.0f}s")
         print()
 
-    def _fit_batch(self, X, y_residual):
+    def fit_residuals(self, X, y_residual):
         theta_0 = self.compute_theta_0(X)
         print(f"computing grads ...")
 
@@ -84,12 +86,8 @@ class MatrixExpEstimator(Estimator):
                     else:
                         ws[i] = ws[i] + cur_w
 
-        pred_change = torch.matmul(theta_0, right_vector)
-
         self.ws.grad = -torch.stack(ws)
         self.optimizer.step()
-
-        return pred_change
 
     def predict(self, X):
         def predict_one(x):