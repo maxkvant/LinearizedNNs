cfe035f24d5bbd27d4fb0b1358f612d86f81ef18
maxkvant <max.kvant@gmail.com>
2020-04-04 04:02:24 +0300

    channel_groups in ccn

--- src/pytorch_impl/nns/cnn.py
+++ src/pytorch_impl/nns/cnn.py
@@ -3,29 +3,32 @@ import torch.nn.functional as F
 
 
 class CNN(nn.Module):
-    def __init__(self, num_classes, input_channels, num_channels=64):
+    def __init__(self, num_classes, input_channels, num_channels=64, channel_groups=1):
         super(CNN, self).__init__()
 
+        self.prepare = nn.Sequential(
+            nn.Conv2d(input_channels, num_channels, kernel_size=3, stride=1, padding=1, bias=False),  # output: 32 x 32 x num_channels
+            nn.BatchNorm2d(num_channels),
+            nn.ReLU(),
+        )
+
         self.layers = nn.Sequential(
-            nn.Conv2d(input_channels, num_channels, kernel_size=3, stride=1, padding=1, bias=False),             # output: 32 x 32
+            nn.Conv2d(num_channels, num_channels, kernel_size=3, stride=1, padding=1, groups=channel_groups, bias=False),  # output: 32 x 32 x num_channels
             nn.BatchNorm2d(num_channels),
             nn.ReLU(),
 
-            nn.Conv2d(num_channels, num_channels, kernel_size=3, stride=1, padding=1, bias=False),  # output: 32 x 32
+            nn.Conv2d(num_channels, num_channels, kernel_size=3, stride=2, padding=1, groups=channel_groups, bias=False),  # output: 16 x 16 x num_channels
             nn.BatchNorm2d(num_channels),
             nn.ReLU(),
 
-            nn.Conv2d(num_channels, num_channels, kernel_size=3, stride=2, padding=1, bias=False),  # output: 16 x 16
-            nn.BatchNorm2d(num_channels),
-            nn.ReLU(),
-
-            nn.Conv2d(num_channels, num_channels, kernel_size=3, stride=2, padding=1, bias=False),  # output:  8 x  8
+            nn.Conv2d(num_channels, num_channels, kernel_size=3, stride=2, padding=1, groups=channel_groups, bias=False),  # output:  8 x  8 x num_channels
             nn.BatchNorm2d(num_channels),
             nn.ReLU()
         )
         self.classifier = nn.Linear(num_channels, num_classes, bias=False)
 
     def forward(self, x):
+        x = self.prepare(x)
         x = self.layers(x)
         x_avg = F.adaptive_avg_pool2d(x, (1, 1))
         x_avg = x_avg.view(x_avg.size(0), -1)
