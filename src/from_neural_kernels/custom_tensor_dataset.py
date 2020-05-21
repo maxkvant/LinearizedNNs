5ff7df1153d5295954b1ea354d5bddb789b8355b
maxkvant <max.kvant@gmail.com>
2020-05-21 20:57:02 +0300

    move augmentation to separate method

--- src/from_neural_kernels/custom_tensor_dataset.py
+++ src/from_neural_kernels/custom_tensor_dataset.py
@@ -12,28 +12,34 @@ Based on part of the following code https://github.com/modestyachts/neural_kerne
 class CustomTensorDataset(torch.utils.data.Dataset):
     """TensorDataset with support of transforms.
     """
+    randomcrop = RandomCrop(32, padding=4)
+    cutout     = Cutout(n_holes=1, length=16)
 
     def __init__(self, data, targets, transform=None):
         assert data.size(0) == targets.size(0)
         self.data = data
         self.targets = targets
         self.transform = transform
-        self.cutout = Cutout(n_holes=1, length=16)
-        self.randomcrop = RandomCrop(32, padding=4)
 
     def __getitem__(self, idx):
         x = self.data[idx]
-        if self.transform == 'flips':
-            if np.random.binomial(1, 0.5) == 1:
-                x = x.flip(2)
-        elif self.transform == 'all':
-            x = self.randomcrop(x)
-            if np.random.binomial(1, 0.5) == 1:
-                x = x.flip(2)
-            x = self.cutout(x)
+        x = self.augment_x(x, self.transform)
 
         y = self.targets[idx]
-        return x.float(), y
+        return x, y
 
     def __len__(self):
         return self.data.size(0)
+
+    @staticmethod
+    def augment_x(x, transform='all'):
+
+        if transform == 'flips':
+            if np.random.binomial(1, 0.5) == 1:
+                x = x.flip(2)
+        elif transform == 'all':
+            x = CustomTensorDataset.randomcrop(x)
+            if np.random.binomial(1, 0.5) == 1:
+                x = x.flip(2)
+            x = CustomTensorDataset.cutout(x)
+        return x.float()
