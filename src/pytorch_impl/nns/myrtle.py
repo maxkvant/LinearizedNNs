39b76c76668d79f69a36b60fb189ed11a994f4be
maxkvant <max.kvant@gmail.com>
2020-05-29 15:57:58 +0300

    new myrtle 7 architecture

--- src/pytorch_impl/nns/myrtle.py
+++ src/pytorch_impl/nns/myrtle.py
@@ -43,21 +43,23 @@ class Myrtle7(nn.Module):
 
         self.layers = nn.Sequential(
             Conv(input_filters, filters * groups), Activation(),
-            Conv(filters, filters, groups), Activation(),
+            Conv(filters, filters * 2, groups),    Activation(),
             nn.AvgPool2d(kernel_size=2, stride=2),
 
-            Conv(filters, filters, groups), Activation(),
-            Conv(filters, filters, groups), Activation(),
+            Conv(filters * 2, filters * 4, groups), Activation(),
+            Conv(filters * 4, filters * 8, groups), Activation(),
+
             nn.AvgPool2d(kernel_size=2, stride=2),
 
-            Conv(filters, filters, groups), Activation(),
-            Conv(filters, filters, groups), Activation(),
+            Conv(filters *  8, filters * 16, groups), Activation(),
+            Conv(filters * 16, filters * 32, groups), Activation(),
+
             nn.AvgPool2d(kernel_size=8, stride=8),
 
             Flatten(),
-            Normalize(filters)
+            Normalize(filters * 32)
         )
-        self.classifier = nn.Linear(filters * groups, num_classes, bias=True)
+        self.classifier = nn.Linear(filters * 32 * groups, num_classes, bias=True)
 
     def readout(self, x):
         return self.layers(x)
